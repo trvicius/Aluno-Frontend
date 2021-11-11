a80f49ec705aa355aef92225cf3863e7d38912fe
Vinicius Galvao de Franca <vinicius.gfranca98@gmail.com>
2021-11-11 12:58:03 -0300

    Commit ENV

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
  
 const api = axios.create({
-    baseURL: 'https://backendaluno.herokuapp.com/'
+    baseURL: process.env.REACT_APP_BACKEND
 })
  
 export default api;
